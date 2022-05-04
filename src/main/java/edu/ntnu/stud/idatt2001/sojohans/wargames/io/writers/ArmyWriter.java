70abcc2a20f5dc86a3061c2825cbce176fcae5e6
Sander Johansen <sander.olin@gmail.com>
2022-05-04 23:53:06 +0200

    Refactored Method to write Units to File with number of occurrences

--- src/main/java/edu/ntnu/stud/idatt2001/sojohans/wargames/io/writers/ArmyWriter.java
+++ src/main/java/edu/ntnu/stud/idatt2001/sojohans/wargames/io/writers/ArmyWriter.java
@@ -1,10 +1,13 @@
 package edu.ntnu.stud.idatt2001.sojohans.wargames.io.writers;
 
 import edu.ntnu.stud.idatt2001.sojohans.Utilities;
+import edu.ntnu.stud.idatt2001.sojohans.wargames.domain.units.Unit;
 import edu.ntnu.stud.idatt2001.sojohans.wargames.domain.war.Army;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Class for writing an Army object to file.
@@ -27,16 +30,14 @@ public class ArmyWriter {
             throw new IOException("Army cannot be null!");
         }
         if (nameOfFile == null || Utilities.shortenAndReplaceUnnecessarySymbolsInString(nameOfFile).isEmpty()){
-            throw new IOException("File name cannot be null or empty");
+            throw new IOException("File name cannot be null or empty!");
         }
         StringBuilder stringBuilder =  new StringBuilder();
         stringBuilder.append(army.getName()).append(NEWLINE)
                 .append(army.getUnits().size()).append(NEWLINE);
 
-        army.getUnits().forEach(unit -> stringBuilder
-                .append(unit.getClass().getSimpleName()).append(DELIMITER)
-                .append(unit.getName()).append(DELIMITER)
-                .append(unit.getHealth()).append(NEWLINE));
+        String unitsToFile = getUnitsWithNumberOfOccurrencesToBeWrittenToFile(army.getUnits());
+        stringBuilder.append(unitsToFile);
 
         try(FileWriter fileWriter = new FileWriter(Utilities.convertStringToFile
                 (Utilities.shortenAndReplaceUnnecessarySymbolsInString(nameOfFile)))){
@@ -45,5 +46,60 @@ public class ArmyWriter {
         } catch (IOException exception){
             throw new IOException("Unable to write Army to file: " + exception.getMessage());
         }
+    }
+
+    /**
+     * Method for writing only Units to an Army File.
+     * @param units List of Units to be added to File.
+     * @param nameOfFile Name of file where Army is to be written.
+     * @throws IOException If List of Units is empty, file name is empty or file cannot be written to.
+     */
+    public static void writeUnitsFromArmyToFile(List<Unit> units, String nameOfFile) throws IOException{
+
+        if (units == null || units.size() == 0){
+            throw new IOException("List of units cannot be null or empty!");
+        }
+        if (nameOfFile == null || Utilities.shortenAndReplaceUnnecessarySymbolsInString(nameOfFile).isEmpty()){
+            throw new IOException("File name cannot be null or empty!");
+        }
+        String unitsToFile = getUnitsWithNumberOfOccurrencesToBeWrittenToFile(units);
+
+        try (FileWriter fileWriter = new FileWriter(Utilities.convertStringToFile(nameOfFile))){
+            fileWriter.write(unitsToFile);
+        } catch (IOException exception){
+            throw new IOException("Unable to write Army to file: " + exception.getMessage());
+        }
+    }
+
+    /**
+     * Method for getting Units with number of occurrences as a String.
+     * Format: Class Name, Name, Health, Attack, Armor, Number of occurrence.
+     * @param units List of Units to be written to String.
+     * @return String of Units to be written to File.
+     * @throws IOException If List of Unit is null or empty.
+     */
+    private static String getUnitsWithNumberOfOccurrencesToBeWrittenToFile(List<Unit> units)
+    throws IOException{
+        if (units == null || units.size() == 0){
+            throw new IOException("List of units cannot be empty");
+        }
+        Map<Unit,Integer> unitsAndNumberOfOccurrences = new HashMap<>();
+        for (Unit unit : units){
+            if (!unitsAndNumberOfOccurrences.containsKey(unit)){
+                unitsAndNumberOfOccurrences.put(unit, 1);
+            } else {
+                unitsAndNumberOfOccurrences.put(unit, unitsAndNumberOfOccurrences.get(unit) + 1);
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        unitsAndNumberOfOccurrences.forEach((unit, integer) ->
+                stringBuilder.append(unit.getClass().getSimpleName()).append(DELIMITER)
+                        .append(unit.getName()).append(DELIMITER)
+                        .append(unit.getHealth()).append(DELIMITER)
+                        .append(unit.getAttack()).append(DELIMITER)
+                        .append(unit.getArmor()).append(DELIMITER)
+                        .append(integer).append(NEWLINE));
+
+        return stringBuilder.toString();
     }
 }
--- src/test/java/edu/ntnu/stud/idatt2001/sojohans/wargames/io/writers/ArmyWriterTest.java
+++ src/test/java/edu/ntnu/stud/idatt2001/sojohans/wargames/io/writers/ArmyWriterTest.java
@@ -1,5 +1,8 @@
 package edu.ntnu.stud.idatt2001.sojohans.wargames.io.writers;
 
+import edu.ntnu.stud.idatt2001.sojohans.wargames.domain.units.CommanderUnit;
+import edu.ntnu.stud.idatt2001.sojohans.wargames.domain.units.InfantryUnit;
+import edu.ntnu.stud.idatt2001.sojohans.wargames.domain.units.RangedUnit;
 import edu.ntnu.stud.idatt2001.sojohans.wargames.domain.war.Army;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
@@ -30,7 +33,28 @@ class ArmyWriterTest {
     @Test
     void runWriteArmyToFileButWithAFileNameThatIsEmpty(){
         Army army = new Army("Army");
+        army.addUnit(new InfantryUnit("Swordsman",100));
+        army.addUnit(new InfantryUnit("Swordsman",100));
+        army.addUnit(new InfantryUnit("SwordsmaN",100));
+        army.addUnit(new RangedUnit("Archer",100));
+        army.addUnit(new RangedUnit("Archer",100));
+        army.addUnit(new RangedUnit("Archer",101));
+        army.addUnit(new CommanderUnit("Archer",101));
         String emptyString = "      ";
         assertThrows(IOException.class, () -> ArmyWriter.writeArmyToFile(army, emptyString));
+    }
+
+    @Test
+    void testSomething() throws IOException{
+        Army army = new Army("Army");
+        army.addUnit(new InfantryUnit("Swordsman",100));
+        army.addUnit(new InfantryUnit("Swordsman",100));
+        army.addUnit(new InfantryUnit("SwordsmaN",100));
+        army.addUnit(new RangedUnit("Archer",100));
+        army.addUnit(new RangedUnit("Archer",100));
+        army.addUnit(new RangedUnit("Archer",101));
+        army.addUnit(new CommanderUnit("Archer",101));
+
+        ArmyWriter.writeArmyToFile(army, army.getName());
     }
 }
